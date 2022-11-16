9f99a2c050d4b135c140d0d6d0dd1df2fbf69b90
Nejern <nejern@yandex.ru>
2022-11-16 16:09:04 +0300

    base.theme has been edited

--- lua/base/theme.lua
+++ lua/base/theme.lua
@@ -1,3 +1,16 @@
+--[[ Переменные ]]--
+
+local opt = vim.opt
+local g = vim.g
+
+
 --[[ Цветовая схема ]]--
+
+opt.termguicolors = true
+opt.background = 'dark'
+
+g.gruvbox_material_background = 'hard'
+g.gruvbox_material_foreground = 'hard'
+g.gruvbox_material_better_performance = 1
 
 vim.cmd [[colorscheme gruvbox-material]]
