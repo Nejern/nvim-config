23f4be441995c4a74df500f25033aea5712f1105
Nejern <nejern@yandex.ru>
2022-11-16 16:29:20 +0300

    Added lsp server "pylsp"

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,5 +1,6 @@
 require("lspconfig").sumneko_lua.setup {}
 require("lspconfig").clangd.setup {}
+require("lspconfig").pylsp.setup {}
 require("mason-lspconfig").setup({
-        ensure_installed = { "clangd", "sumneko_lua" }
+        ensure_installed = { "clangd", "sumneko_lua", "pylsp" }
     })
